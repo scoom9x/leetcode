d7579f07ce08a2911a968143189816373bc21a82
Zenny <joshzhou20080919@gmail.com>
2026-08-02 00:28:53 +0800

    fixed code for intuitive 242

--- 242/intuitive.py
+++ 242/intuitive.py
@@ -1,3 +1,9 @@
-s = "rac"
-s2 = sorted(s)
-print(s2)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        containsMap = {"a":0, "b":0, "c": 0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, "l":0,"m":0, "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, "w":0, "x":0, "y":0, "z":0}
+        containsMap2 = {"a":0, "b":0, "c": 0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, "l":0,"m":0, "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, "w":0, "x":0, "y":0, "z":0}
+        for k in s:
+            containsMap[k] += 1
+        for k in t:
+            containsMap2[k] += 1
+        return containsMap == containsMap2
