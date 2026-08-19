52adae8597bbcf6a88260dee7aea3fa5248664a6
Josh Zhou <joshzhou20080919@gmail.com>
2026-08-19 15:30:01 -0400

    feat: added merge sort sol

---
+++ 912/merge_sort.py
@@ -0,0 +1,38 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+
+        # Recursion
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        # Merge
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] <= right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+
+numbers = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(numbers)
+print(numbers)
