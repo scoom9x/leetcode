56ce4c7dbba0d4a55d9965f964b69a0cc69e1f0c
Josh Zhou <joshzhou20080919@gmail.com>
2026-08-29 23:09:01 -0400

    Time: 0ms(100.0%), Space: 12.19MB(99.65%)

--- 75/intuitive.py
+++ 75/intuitive.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def sortColors(self, nums):
-        r, w, b = 0, 0, 0
+        r, w = 0, 0
         for n in nums:
             if n == 0:
                 r += 1
             elif n == 1:
                 w += 1
-            else:
-                b += 1
                 
         for i in range(len(nums)):
             if r > 0:
@@ -18,7 +16,5 @@ class Solution(object):
                 nums[i] = 1
                 w -= 1
             
-            elif b > 0:
+            else:
                 nums[i] = 2
-                b -= 1
-        
